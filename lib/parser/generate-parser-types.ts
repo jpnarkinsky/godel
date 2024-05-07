9e4107e3687c583b051fc141058d870f8c98d627
jpn-helios <141862493+jpn-helios@users.noreply.github.com>
2024-05-07 17:14:21 +0000

    cleanup

--- lib/parser/generate-parser-types.ts
+++ lib/parser/generate-parser-types.ts
@@ -6,8 +6,6 @@
 import { writeFileSync } from "fs";
 import { generateCstDts } from "chevrotain";
 import { productions } from "./parser.ts";
-// import { fileURLToPath } from "url";
 
 const dtsString = generateCstDts(productions);
-// const dtsPath = resolve("lib", "parser", "parser.d.ts");
 writeFileSync("lib/parser/parser.d.ts", dtsString);
